b76308590359a1a35e0ca2369ab136c866956f57
SOUMEN JANA <soumyaroy13336@gmail.com>
2024-06-08 19:23:37 +0530

    fix backend issue

--- job-portal-server/api/index.js
+++ job-portal-server/api/index.js
@@ -21,10 +21,10 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
+// async function run() {
+//   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     // create database
     const db = client.db("mernJobPortal");
@@ -46,11 +46,11 @@ async function run() {
         }
     })
 
-    // // get all jobs
-    // app.get("/all-jobs", async(req, res) => {
-    //     const jobs = await jobsCollections.find({}).toArray()
-    //     res.send(jobs);
-    // })
+    // get all jobs
+    app.get("/all-jobs", async(req, res) => {
+        const jobs = await jobsCollections.find({}).toArray()
+        res.send(jobs);
+    })
 
     // get single job using id
     app.get("/all-jobs/:id" , async(req, res) => {
@@ -95,29 +95,14 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+  // } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-
-
-// get all jobs
-app.get("/all-jobs", async(req, res) => {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-
-      // create database
-      const db = client.db("mernJobPortal");
-      const jobsCollections = db.collection("demoJobs")
-  const jobs = await jobsCollections.find({}).toArray()
-  res.send(jobs);
-})
+  // }
+// }
+// run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
